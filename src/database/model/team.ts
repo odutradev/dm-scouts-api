5bf4b0877efc0e36600bf519f28dbfe12a4e5c4c
odutradev <joao.vitornl@gmail.com>
2025-03-21 17:06:07 -0300

    feat: Configure team

---
+++ src/database/model/team.ts
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const teamSchema = new mongoose.Schema({
+    name: String,
+    group: Number,
+    leader: {
+        id:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "user"
+        },
+        name: String
+    }, 
+    status: {
+        enum: ["active", "inactive"],
+        default: "active",
+        type: String,
+    },
+    branch: {
+        enum: ["wolfcub", "scout", "senior", "pioneer"],
+        type: String,
+    },
+    local: String,
+    createAt: {
+        default: Date.now(),
+        type: Date,
+    },
+    lastUpdate: {
+        type: Date,
+    },
+    description: {
+        type: String
+    },
+    images: {
+        profile: String
+    }
+});
+
+const teamModel = mongoose.model("team", teamSchema);
+
+export default teamModel;
