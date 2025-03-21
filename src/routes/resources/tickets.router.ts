d2520c4ebf0c90e3fb451de3c56363830adc7c46
odutradev <joao.vitornl@gmail.com>
2025-03-21 11:48:43 -0300

    Configure ticket routes

--- src/routes/resources/tickets.router.ts
+++ src/routes/resources/tickets.router.ts
@@ -7,7 +7,6 @@ const ticketsRouter = Router();
 
 ticketsRouter.patch("/:ticketID/messages/add", manageRequest(ticketsResource.addTicketMessage));
 ticketsRouter.patch("/:ticketID/update", manageRequest(ticketsResource.updateTicket));
-ticketsRouter.get("/space/:spaceID", manageRequest(ticketsResource.getSpaceTickets));
 ticketsRouter.get("/system", manageRequest(ticketsResource.getSystemTickets));
 ticketsRouter.get("/:ticketID", manageRequest(ticketsResource.getTicket));
 ticketsRouter.post("/create", manageRequest(ticketsResource.createTicket));
