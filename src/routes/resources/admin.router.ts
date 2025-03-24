c5811d2c077689806dfb5dbdd8f10ffa25ee2519
odutradev <joao.vitornl@gmail.com>
2025-03-24 17:28:50 -0300

    feat: Create admin resouce routes

--- src/routes/resources/admin.router.ts
+++ src/routes/resources/admin.router.ts
@@ -5,10 +5,14 @@ import manageRequest from "@middlewares/manageRequest";
 
 const adminRouter = Router();
 
-adminRouter.delete("/users/delete/:userID", manageRequest(adminResource.deleteUser));
-adminRouter.patch("/users/update/:userID", manageRequest(adminResource.updateUser));
+adminRouter.patch("/users/:userID/update/password", manageRequest(adminResource.updateUserPassword));
+adminRouter.delete("/users/:userID/delete/", manageRequest(adminResource.deleteUser));
+adminRouter.patch("/users/:userID/update", manageRequest(adminResource.updateUser));
 adminRouter.post("/users/create", manageRequest(adminResource.createUser));
 adminRouter.get("/users/:userID", manageRequest(adminResource.getUser));
 adminRouter.get("/users", manageRequest(adminResource.getAllUsers));
+
+adminRouter.patch("/config/update", manageRequest(adminResource.updateConfig));
+adminRouter.get("/config", manageRequest(adminResource.getConfig));
 
 export default adminRouter;
