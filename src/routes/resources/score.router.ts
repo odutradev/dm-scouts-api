5584f56328edc5c29718df5f6a53e05491ce9791
odutradev <joao.vitornl@gmail.com>
2025-04-04 18:02:45 -0300

    fix: Fix use hasAdmin

--- src/routes/resources/score.router.ts
+++ src/routes/resources/score.router.ts
@@ -2,12 +2,11 @@ import { Router } from "express";
 
 import scoreResource from "@resources/score/score.resource";
 import manageRequest from "@middlewares/manageRequest";
-import hasAdmin from "@middlewares/hasAdmin";
 
 const scoresRouter = Router();
 
 scoresRouter.patch("/:scoreID/update/leader-confirm", manageRequest(scoreResource.updateTeamLeaderConfirm));
-scoresRouter.post("/create", [hasAdmin], manageRequest(scoreResource.createScore));
+scoresRouter.post("/create", manageRequest(scoreResource.createScore));
 scoresRouter.get("/user/:userID", manageRequest(scoreResource.getUserScores));
 scoresRouter.get("/team/:teamID", manageRequest(scoreResource.getTeamScores));
 scoresRouter.get("/base/:baseID", manageRequest(scoreResource.getBaseScores));
